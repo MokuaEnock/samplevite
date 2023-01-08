1505bda55ede7c24c0f2cb7820e9c6f79cda706f
Enock <enock.mokua@student.moringaschool.com>
2023-01-08 03:09:22 +0300

    grab input data from signup forms

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -8,15 +8,15 @@ export default function Login() {
 
     return <main id="login">
         <form id="form1">
-            <input type="email" placeholder="email" />
-            <input type="text" placeholder="name" />
+            <input type="email" placeholder="email" value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="text" placeholder="name" value={name} onChange={e => setName(e.target.value)} />
             <button type="submit">Submit</button>
 
         </form>
 
         <form id="form2">
-            <input type="number" placeholder="Number 1" />
-            <input type="number" placeholder="Number 1" />
+            <input type="number" placeholder="Number 1" value={first} onChange={e => setFirst(e.target.value)} />
+            <input type="number" placeholder="Number 1" value={second} onChange={e => setSecond(e.target.value)} />
             <button type="submit">Submit</button>
         </form>
     </main>
